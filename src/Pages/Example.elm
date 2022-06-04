c67c0a318b87b747d1d4ea263324626a4e89c0f9
Stephan <stephan.dint.mueller@gmail.com>
2022-06-04 13:13:30 +0200

    Example struggle

---
+++ src/Pages/Example.elm
@@ -0,0 +1,71 @@
+module Pages.Example exposing (Model, Msg, page)
+
+import Gen.Params.Example exposing (Params)
+import Html
+import Html.Events
+import Page
+import Request
+import Shared
+import View exposing (View)
+
+
+
+-- When I do elm-spa add /example sandbox then page : is created Request.With Params
+-- The example in documentation does not have this. The page has many errors
+
+
+page : Shared.Model -> Request -> Page.With Model Msg
+page =
+    Page.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
+
+
+
+-- INIT
+
+
+type alias Model =
+    { counter : Int }
+
+
+init : Model
+init =
+    { counter = 0 }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            { model | counter = model.counter + 1 }
+
+        Decrement ->
+            { model | counter = model.counter - 1 }
+
+
+
+-- VIEW
+
+
+view : Model -> View Msg
+view model =
+    { title = "Homepage"
+    , body =
+        [ Html.h1 [] [ Html.text "Local storage" ]
+        , Html.button [ Html.Events.onClick Increment ] [ Html.text "+" ]
+        , Html.p [] [ Html.text ("Count: " ++ String.fromInt model.counter) ]
+        , Html.button [ Html.Events.onClick Decrement ] [ Html.text "-" ]
+        ]
+    }
