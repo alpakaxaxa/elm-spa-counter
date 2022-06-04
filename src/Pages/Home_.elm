a8c2571bbf8d4411378b8283b4cae9693adc6abd
Stephan <stephan.dint.mueller@gmail.com>
2022-06-04 13:24:27 +0200

    Example update

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -1,14 +1,28 @@
 module Pages.Home_ exposing (Model, Msg, page)
 
+import ElmSpa.Request exposing (Request)
 import Gen.Params.Home_ exposing (Params)
+import Html
+import Html.Events
 import Page
 import Request
 import Shared
 import View exposing (View)
 
 
-page : Shared.Model -> Request.With Params -> Page.With Model Msg
-page shared req =
+
+{- page : Shared.Model -> Request.With Params -> Page.With Model Msg
+   page shared req =
+       Page.sandbox
+           { init = init
+           , update = update
+           , view = view
+           }
+-}
+
+
+page : Shared.Model -> Request -> Page.With Model Msg
+page =
     Page.sandbox
         { init = init
         , update = update
@@ -21,12 +35,12 @@ page shared req =
 
 
 type alias Model =
-    {}
+    { counter : Int }
 
 
 init : Model
 init =
-    {}
+    { counter = 0 }
 
 
 
@@ -34,14 +48,18 @@ init =
 
 
 type Msg
-    = ReplaceMe
+    = Increment
+    | Decrement
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        ReplaceMe ->
-            model
+        Increment ->
+            { model | counter = model.counter + 1 }
+
+        Decrement ->
+            { model | counter = model.counter - 1 }
 
 
 
@@ -50,4 +68,11 @@ update msg model =
 
 view : Model -> View Msg
 view model =
-    View.placeholder "Home_"
+    { title = "Homepage"
+    , body =
+        [ Html.h1 [] [ Html.text "Local storage" ]
+        , Html.button [ Html.Events.onClick Increment ] [ Html.text "+" ]
+        , Html.p [] [ Html.text ("Count: " ++ String.fromInt model.counter) ]
+        , Html.button [ Html.Events.onClick Decrement ] [ Html.text "-" ]
+        ]
+    }
